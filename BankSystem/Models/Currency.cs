d8b414934768aa9d1c9a8f3f0465984c7fef9ffe
Dima Ballanar <D.Ballanar@gmail.com>
2023-01-17 20:22:56 +0300

    update

--- BankSystem/Models/Currency.cs
+++ BankSystem/Models/Currency.cs
@@ -2,6 +2,12 @@ namespace BankSystem.Models;
 
 public class Currency
 {
+    public int Code { get; set; }
+    public string Name { get; set; }
+    public string Symbol { get; set; }
+    public string ShortName { get; set; }
 
 
 }
+
+// Код валюты, имя валюты, символ валюты, сокращенное имя 
